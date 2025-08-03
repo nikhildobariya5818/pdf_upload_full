887581da513cf8fcbd1c39ee447fd0c2ee4a8257
romil24 <romilchodavadiya20@gmail.com>
2025-08-03 18:11:34 +0530

    Fixes

--- client/src/components/InvoicePDFSection5.tsx
+++ client/src/components/InvoicePDFSection5.tsx
@@ -4,8 +4,8 @@ import { BASE_URL } from "./ReportProcessor";
 
 export default function InvoicePDFSection5({ data }: { data: any }) {
     return (
-        <View>
-            {/* QR Code Section (DO NOT TOUCH) */}
+        <View style={styles.container}>
+            {/* QR Code */}
             <View style={styles.qrContainer}>
                 <Image
                     src={`${BASE_URL}/files/qrcode.png`}
@@ -13,19 +13,14 @@ export default function InvoicePDFSection5({ data }: { data: any }) {
                 />
             </View>
 
-
-            {/* <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: '-14px' }}> */}
-            <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginTop: '-14px', marginRight: '10px' }}>
+            {/* Barcode and number */}
+            <View style={styles.barcodeRow}>
                 <Image
                     src={`${BASE_URL}/files/barcode10.png`}
-                    style={{
-                        width: 98,
-                        height: 10,
-                        objectFit: "contain",
-                    }}
+                    style={styles.barcodeImage}
                 />
-                <Text style={[commonStyles.ReportDate, { marginLeft: 2 }]}>
-                    {data.BARCODE10.number}
+                <Text style={styles.barcodeText}>
+                    {data?.BARCODE10?.number ?? ""}
                 </Text>
             </View>
         </View>
@@ -33,53 +28,38 @@ export default function InvoicePDFSection5({ data }: { data: any }) {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        marginTop: 420, // space from previous section (adjust if needed)
+        alignItems: "center",
+    },
     qrContainer: {
-        marginTop: 406.9,
-        width: 100,
-        height: 100,
-        marginLeft: 135,
+        width: 60,
+        height: 59,
+        marginBottom: '33px',
+        marginLeft: 144,
+        // backgroundColor: 'red'
     },
     qrImage: {
         width: "100%",
         height: "100%",
         objectFit: "contain",
     },
-    // barcodeContainer: {
-    //     marginLeft: 30,
-    //     marginTop: -47,
-    //     width: '97px',
-    //     height: '81px',
-    // },
-    barcodeContainer: {
-        position: 'absolute',
-        top: 460,
-        left: 30,
-        width: '97px',
-        height: '81px',
+    barcodeRow: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 4,
     },
-    // barcodeContainer: {
-    //     marginLeft: 30,
-    //     marginTop: -47,
-    //     width: '97px',
-    //     height: '81px',
-    //     justifyContent: 'center',
-    // },
-    // barcodeImage: {
-    //     width: 60, // adjust as needed
-    //     height: 40,
-    //     objectFit: 'contain',
-    // },
-    barcodeText: {
-        fontSize: 8,
-        marginLeft: 6,
-    },
-    // barcodeText: {
-    //     fontSize: 6,
-    //     marginBottom: 2,
-    // },
     barcodeImage: {
-        width: "100%",
-        height: "80%",
+        width: 98,
+        height: 10,
         objectFit: "contain",
+    },
+    barcodeText: {
+        marginLeft: 1,
+        marginTop: '0.70px',
+        fontSize: "8.80",
+        fontFamily: "OCR",
+        color: "#000",
     },
 });

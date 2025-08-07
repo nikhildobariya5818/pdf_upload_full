8738615490a612f690cc4d94baf0f7804fcc7e74
Nikhil Dobariya <133102352+nikhildobariya5818@users.noreply.github.com>
2025-08-07 11:35:12 +0530

    Update multi_diamond.py

--- backend/multi_diamond.py
+++ backend/multi_diamond.py
@@ -168,11 +168,12 @@ def process_gia_pdf(pdf_path):
         "Measurements": find_value(r"Measurements \.+ (.+)"),
     }
     grading_results = {
-    "CaratWeight": find_value(r"Carat Weight\s*\.\s(.+)"),
-    "ColorGrade": find_value(r"Color Grade\s*\.\s(.+)"),
-    "ClarityGrade": find_value(r"Clarity Grade\s*\.\s(.+)"),
-    "CutGrade": find_value(r"Cut Grade\s*\.\s(.+)"),
-    }
+    "CaratWeight": find_value(r"Carat Weight \.+ (.+)"),
+    "ColorGrade": find_value(r"Color Grade \.+ (.+)"),
+    "ClarityGrade": find_value(r"Clarity Grade \.+ (.+)"),
+    "CutGrade": find_value(r"Cut Grade \.+ (.+)"),
+}
+
     additional_info = {
         "polish": find_value(r"Polish \.+ (.+)"),
         "symmetry": find_value(r"Symmetry \.+ (.+)"),
